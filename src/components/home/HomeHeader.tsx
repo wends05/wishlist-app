4e43416d2d35daf1c8650e26589dd352d9a5cdac
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-10-14 19:19:55 +0800

    added an add wish button on the header, and appears when sidebar is not open

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,14 +1,29 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { getRouteTitle, homeLinks } from "@/utils/links";
-import { SidebarTrigger } from "../ui/sidebar";
+import { Button } from "../ui/button";
+import { SidebarTrigger, useSidebar } from "../ui/sidebar";
 
 const HomeHeader = () => {
+  const { open } = useSidebar();
   const pathName = usePathname();
   return (
-    <main className="fixed flex items-center gap-4 px-3 py-2">
-      <SidebarTrigger /> <h3>{getRouteTitle(pathName, homeLinks)}</h3>
+    <main className="pt-2">
+      <div className="fixed flex items-center gap-4">
+        <SidebarTrigger /> <h3>{getRouteTitle(pathName, homeLinks)}</h3>
+      </div>
+      {!open && (
+        <Link href={"/create"}>
+          <Button
+            variant={"outline"}
+            className="fixed right-8 hover:text-primary"
+          >
+            Add a wish
+          </Button>
+        </Link>
+      )}
     </main>
   );
 };
