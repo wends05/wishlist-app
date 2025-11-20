949b61ecc51f6207297e644474b72d62877ee244
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-11-21 00:12:39 +0800

    updated the edit profile details

--- src/app/(protected)/(main)/profile/edit/EditProfilePage.tsx
+++ src/app/(protected)/(main)/profile/edit/EditProfilePage.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { type Preloaded, usePreloadedQuery } from "convex/react";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import EditProfileForm from "@/forms/EditProfileForm";
 import type { api } from "../../../../../../convex/_generated/api";
 
 interface EditProfilePageProps {
-  preloadedProfileDetails: Preloaded<typeof api.users.getCurrentUserDataHandler>;
+  preloadedProfileDetails: Preloaded<
+    typeof api.users.getCurrentUserDataHandler
+  >;
 }
 export default function EditProfilePage({
   preloadedProfileDetails,
@@ -13,7 +17,12 @@ export default function EditProfilePage({
   const profileDetails = usePreloadedQuery(preloadedProfileDetails);
   return (
     <main className="flex h-full w-full flex-col px-4 pt-12">
-      <h2>Edit Profile Page</h2>
+      <div className="flex items-center gap-2">
+        <Link href={"/profile"}>
+          <ArrowLeft />
+        </Link>
+        <h2>Edit Profile Page</h2>
+      </div>
       <EditProfileForm
         defaultValues={{
           name: profileDetails.name,
